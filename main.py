6ae339c3b03fe715b978a80450971b9e84801fc8
justinelm <88989785+justinelm@users.noreply.github.com>
2022-05-25 14:31:49 +0800

    fix: reformat code

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#import pyautogui
+# import pyautogui
 import pyautogui
 
-#run shift + F10
-#call position function to get position of the mouse
-#Log the result
-#Result: Point(x=394, y=70)
+# run shift + F10
+# call position function to get position of the mouse
+# Log the result
+# Result: Point(x=394, y=70)
 
 print(pyautogui.position())
 
-#click to focus on the searchbar
+# click to focus on the searchbar
 pyautogui.click(394, 70)
 
-#type hello world
+# type hello world
 pyautogui.typewrite("Hello world")
 
-#click enter
+# click enter
 pyautogui.typewrite(["enter"])
