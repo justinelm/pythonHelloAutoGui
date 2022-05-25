447f3bbd3ba3fe249a726593614b0b3d314a7cfd
justinelm <88989785+justinelm@users.noreply.github.com>
2022-05-25 14:52:07 +0800

    feat: allow new tab creation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyautogui
 
 # run shift + F10
 # call position function to get position of the mouse
-# Log the result
+# Log the outcome
 # Result: Point(x=394, y=70)
 
 print(pyautogui.position())
@@ -13,6 +13,25 @@ pyautogui.click(394, 70)
 
 # type hello world
 pyautogui.typewrite("Hello world")
+
+# click enter
+pyautogui.typewrite(["enter"])
+
+# get the Google URL
+# focus on the input
+pyautogui.click(394, 70)
+
+# copy
+pyautogui.hotkey("ctrl", "c")
+
+# open new tab using ctrl + t
+pyautogui.hotkey("ctrl", "t")
+
+# focus on new search bar
+pyautogui.click(394, 70)
+
+# paste the URL
+pyautogui.hotkey("ctrl", "v")
 
 # click enter
 pyautogui.typewrite(["enter"])
